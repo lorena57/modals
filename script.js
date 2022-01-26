0db7d01ecb816c7bb7c3b9f8b68297566f502e1c
Lorena C <lorena@eunknown.org>
2022-01-25 19:02:50 -0800

    create open modal function

--- script.js
+++ script.js
@@ -8,17 +8,19 @@ const btnCloseModal = document.querySelector('.close-modal');
 
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', function () {
-    modal.classList.remove('hidden');
-    overLay.classList.remove('hidden');
-  });
-}
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overLay.classList.remove('hidden');
+};
 
 const closeModal = function () {
   modal.classList.add('hidden');
   overLay.classList.add('hidden');
 };
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
 
 btnCloseModal.addEventListener('click', closeModal);
 
