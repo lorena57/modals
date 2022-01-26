0d2badc46586e0565f8d21f1f7c3e1e360ec619f
Lorena C <lorena@eunknown.org>
2022-01-25 18:49:35 -0800

    create function to remove css

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+
+const overLay = document.querySelector('.overlay');
+
+const btn = document.querySelector('.close-modal');
+
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', function () {
+    modal.classList.remove('hidden');
+    overLay.classList.remove('hidden');
+  });
+}
